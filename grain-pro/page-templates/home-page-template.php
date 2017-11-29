98cdd7e120a3f0f49a164bb5e5881093b8322de6
Sergey <shimopus@gmail.com>
2017-11-30 00:58:21 +0300

    Table Page Template added

--- grain-pro/page-templates/home-page-template.php
+++ grain-pro/page-templates/home-page-template.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Home Page
+ * Template Name: Домашняя страница
  */
 
 get_header(); ?>
---
+++ grain-pro/page-templates/table-template.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: Таблица заявок
+ */
+
+get_header(); ?>
+
+    <h1><?php the_field('bids_type'); ?></h1>
+
+<?php
+get_sidebar();
+get_footer();
+
