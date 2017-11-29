a86f1499224aae08997c4f699c281002531e2418
Sergey <shimopus@gmail.com>
2017-11-30 00:06:55 +0300

    Home Page Template added

---
+++ grain-pro/page-templates/home-page-template.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Template Name: Home Page
+ */
+
+get_header(); ?>
+
+
+    <section class="gn-infoblock-time">
+        <div class="gn-page-row">
+            <div class="gn-infoblock-time__content">
+                <h2 class="gn-infoblock-time__header">
+                    Grain.pro —  это сервис для поиска, сравнения, покупки и продажи пшеницы по всей России
+                </h2>
+
+                <div class="gn-infoblock-time__content-inner">
+                    <div class="gn-infoblock-time__advantage">Экономит ваше время</div>
+
+                    <ul class="gn-infoblock-time__list gn-list-u">
+                        <li class="gn-list-u__item">Актуальный список объявлений купли продажи пшеницы</li>
+                        <li class="gn-list-u__item">Открытая и полная информация по каждому объявлению включая карту анализа</li>
+                        <li class="gn-list-u__item">Быстрое сравнение цен от разных компаний на нужный базис</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="gn-infoblock-why">
+        <div class="gn-page-row">
+
+            <div class="gn-infoblock-why__content">
+                <div class="gn-infoblock-why__left">
+
+                    <h2 class="gn-infoblock-why__header">
+                        Зачем Grain.pro<br>
+                        <a class="gn-tabs-link _active" href="#sellers">продавцам</a>
+                        и
+                        <a class="gn-tabs-link" href="#buyers">покупателям</a>
+                    </h2>
+
+                    <div class="gn-infoblock-why__button">
+                        <button class="gn-button">Посмотреть объявления</button>
+                    </div>
+                </div>
+                <div class="gn-infoblock-why__right">
+
+                    <ul class="gn-infoblock-why__list gn-list-i">
+                        <li class="gn-list-i__item _binoculars">Показывает покупателей, которым подходит ваше предложение по цене с учетом стоимости доставки</li>
+                        <li class="gn-list-i__item _wheat">Позволяет легко оценить ваше предложение относительно других продавцов</li>
+                        <li class="gn-list-i__item _plane">Проводит рассылку вашего объявления по  покупателям</li>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="gn-infoblock-distrib">
+        <div class="gn-page-row">
+            <h2 class="gn-infoblock-distrib__header">
+                Рассылка объявлений
+            </h2>
+
+            <div class="gn-infoblock-distrib__content">
+                <div class="gn-infoblock-distrib__left">
+                    <div class="gn-infoblock-distrib__text">
+                        Каждый <a class="gn-link-accent" href="#">понедельник</a> мы проводим рассылку актуальных объявлений по электронной почте. Это удобный способ следить за изменениями цены пшеницы на рынке и быть в курсе текущего спроса и предложения на нужный базис.
+                    </div>
+
+                    <div class="gn-infoblock-distrib__button">
+                        <button class="gn-button">Получать рассылку</button>
+                    </div>
+                </div>
+                <div class="gn-infoblock-distrib__right"></div>
+            </div>
+        </div>
+    </section>
+    <section class="gn-infoblock-advantages">
+        <div class="gn-page-row">
+            <div class="gn-infoblock-advantages__content">
+                <h2 class="gn-infoblock-advantages__header">
+                    Почему стоит разместить объявление на GRAIN.PRO
+                </h2>
+
+                <ul class="gn-infoblock-advantages__list gn-list-u _big-indent">
+                    <li class="gn-list-u__item">Ваше объявление получат более 1500 покупателей и продавцов пшеницы</li>
+                    <li class="gn-list-u__item">Полнота информации в объявлениях увеличит количество теплых звонков</li>
+                    <li class="gn-list-u__item">Инструмент расчета стоимости цены с доставкой расширяет охват потенциальных клиентов</li>
+                </ul>
+
+                <div class="gn-infoblock-advantages__button">
+                    <button class="gn-button">Подать объявление</button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<?php
+get_sidebar();
+get_footer();
+
