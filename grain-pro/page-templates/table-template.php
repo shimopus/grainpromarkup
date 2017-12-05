0a522915e83205d3efa1f1845b8afbe45a095831
Sergey <shimopus@gmail.com>
2017-12-05 12:04:32 +0300

    Table template update.

--- grain-pro/page-templates/table-template.php
+++ grain-pro/page-templates/table-template.php
@@ -3,9 +3,21 @@
  * Template Name: Таблица заявок
  */
 
-get_header(); ?>
+get_header();
 
-    <h1><?php the_field('bids_type'); ?></h1>
+$bidTypeField = get_field_object('bids_type');
+$bidTypeValue = $bidTypeField['value'];
+$bidTypeKey = $bidTypeField['choices'][$bidTypeValue];
+
+$api_request = "https://grainpro.herokuapp.com/pages/market-table/site?bidType=".$bidTypeKey;
+
+$tableHTML = wp_remote_retrieve_body(wp_remote_get($api_request));
+
+print $tableHTML;
+
+?>
+
+
 
 <?php
 get_sidebar();
