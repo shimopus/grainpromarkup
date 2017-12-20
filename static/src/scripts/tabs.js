633a6e81839d2fe4d81a9de10671e3a5fda04750
acer <aleynik.serj@gmail.com>
2017-12-20 21:43:06 +0300

    popup for messages

--- static/src/scripts/tabs.js
+++ static/src/scripts/tabs.js
@@ -1,45 +1,43 @@
 (function() {
 
-    var wrappers = document.querySelectorAll(".jsTabsWrapper"),
-        forEach = Array.prototype.forEach;
+    var forEach = Array.prototype.forEach;
 
+    document.addEventListener("click", function(event) {
+        var target = event.target,
+            targetTabsName,
+            targetTabsGroup,
+            tabs,
+            links,
+            targetTabs;
 
-    forEach.call(wrappers, function(wrapper) {
-        wrapper.addEventListener("click", function(event) {
-            var target = event.target,
-                targetTabsName,
-                tabs,
-                links,
-                targetTabs;
+        if (target.classList.contains("jsTabsLink")) {
+            targetTabsName = target.dataset['tab'];
+            targetTabsGroup = target.dataset['tab-group'];
+            tabs = document.querySelectorAll(".jsTabsTab");
+            links = document.querySelectorAll(".jsTabsLink");
 
-            if (target.classList.contains("jsTabsLink") || target.closest(".jsTabsLink")) {
-                targetTabsName = target.getAttribute('href');
-                tabs = wrapper.querySelectorAll(".jsTabsTab");
-                links = wrapper.querySelectorAll(".jsTabsLink");
-
-                forEach.call(tabs, function(tab) {
+            forEach.call(tabs, function(tab) {
+                if (!targetTabsGroup || tab.dataset['tab-group'] === targetTabsGroup) {
                     if (tab.dataset['tab'] === targetTabsName) {
                         tab.classList.add("_active");
                     } else {
                         tab.classList.remove("_active");
                     }
-                });
+                }
+            });
 
-                forEach.call(links, function(link) {
-                    if (link === target) {
+            forEach.call(links, function(link) {
+                if (!targetTabsGroup || link.dataset['tab-group'] === targetTabsGroup) {
+                    if (link.dataset['tab'] === targetTabsName) {
                         link.classList.add("_active");
                     } else {
                         link.classList.remove("_active");
                     }
-                });
-            }
+                }
+            });
 
             event.preventDefault();
-        });
+        }
     });
-
-
-
-
 
 }) ();
