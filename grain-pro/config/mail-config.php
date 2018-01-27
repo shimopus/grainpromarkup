9ae217dd0c4763f2cf4f31e48f9863fc8b20648d
Sergey <shimopus@gmail.com>
2018-01-27 17:14:11 +0300

    fix wordpress credential percolation

--- grain-pro/config/mail-config.php
+++ grain-pro/config/mail-config.php
@@ -3,12 +3,12 @@
 add_action( 'phpmailer_init', 'send_smtp_email' );
 function send_smtp_email( $phpmailer ) {
 	$phpmailer->isSMTP();
-	$phpmailer->Host       = 'smtp.mailgun.org';
-	$phpmailer->SMTPAuth   = true;
-	$phpmailer->Port       = 587;
-	$phpmailer->Username   = 'postmaster@mailer.grain.pro';
-	$phpmailer->Password   = 'd2c1636ee9a3d0fd3b7db7131b887c95';
-	$phpmailer->SMTPSecure = 'tls';
-	$phpmailer->From       = 'site@grain.pro';
-	$phpmailer->FromName   = 'Grain.pro WordPress';
+	$phpmailer->Host       = SMTP_HOST;
+	$phpmailer->SMTPAuth   = SMTP_AUTH;
+	$phpmailer->Port       = SMTP_PORT;
+	$phpmailer->Username   = SMTP_USER;
+	$phpmailer->Password   = SMTP_PASS;
+	$phpmailer->SMTPSecure = SMTP_SECURE;
+	$phpmailer->From       = SMTP_FROM;
+	$phpmailer->FromName   = SMTP_FROM_NAME;
 }
